c75acdbe8939b43560ce8e9bf7f4401a0d866b88
Daniel Qiu <daniel@danielqiu.com>
2014-09-11 21:17:47 -0700

    fixes to work with Meteor0.9.1.1

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 meteor-user-stream
 =================
 
-Meteor smart package for [user-stream](https://github.com/aivis/user-stream).
+Meteor package for [user-stream](https://github.com/aivis/user-stream).
+
+#Usage
+
+Install package from Meteor:
+```
+meteor add mrt:user-stream 
+
+```
--- package.js
+++ package.js
@@ -1,11 +1,18 @@
 Package.describe({
-	summary: "Simple Twitter (API 1.1) user stream client in meteor"
+	name: "mrt:user-stream",
+	summary: "Simple Twitter (API 1.1) user stream client in meteor",
+	author: "Daniel Qiu",
+	version: "0.0.7",
+	git: "https://github.com/danielqiu/meteor-user-stream.git"
 });
 
 Npm.depends({'user-stream': '0.0.7'});
 
 Package.on_use(function(api){
+	api.versionsFrom('METEOR@0.9.1.1');
+
 	api.add_files(['user-stream.js'], 'server');
+	
 	if(api.export)
 		api.export('Stream');
 });
